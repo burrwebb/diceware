fe80d755f39fcc4486d79f21426f1e433f2a7069
Burr Webb <burrwebb@macbook-air.local>
2016-05-08 22:53:45 -0700

    debugging

--- generateNumber.py
+++ generateNumber.py
@@ -13,7 +13,7 @@ finalCode = str(finalCode)
 
 for x in range(5):
     
-    anInt = random.randint(1,6)
+    anInt = random.randint(1,6),
     anInt = str(anInt)
     finalCode = finalCode + anInt
 
