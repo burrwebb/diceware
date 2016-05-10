6dca63ada1bccc38f79d32c0f46e5d29dd0a22ec
Burr Webb <burrwebb@macbook-air.local>
2016-05-09 19:14:42 -0700

    working on translation

--- generateNumber.py
+++ generateNumber.py
@@ -29,7 +29,7 @@ with open('dicewareList.csv') as csvfile:
       for row in reader:
           idNumber = int(row["idNumber"])
           associatedWord = str(row["associatedWord"])
-          if (int(finalCode) == idNumber)
+              if int(finalCode) == idNumber:
                 print "Word: " + associatedWord
 
 
