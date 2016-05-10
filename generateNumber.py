62bd5ad7918e308a3f2415af0cd72a28eeaae898
Burr Webb <burrwebb@macbook-air.local>
2016-05-09 18:50:08 -0700

    debugging

--- generateNumber.py
+++ generateNumber.py
@@ -24,12 +24,20 @@ for x in range(5):
 print "Number: " + finalCode
 
 
+import csv
+ with open('dicewareList.csv') as csvfile:
+     reader = csv.DictReader(csvfile)
+     for row in reader:
+         
+         print(row['first_name'], row['last_name'])
 
 
-with open('dicewareList.csv') as csvfile:
-     fileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
-     for row in fileReader:
-        print row
+
+
+#with open('dicewareList.csv') as csvfile:
+#     fileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+#     for row in fileReader:
+#        print row
          
         # print(', '.join(row))
 
