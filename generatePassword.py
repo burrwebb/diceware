dbdbc0691f1030f7d6b353c1042fcc3512bb4939
Burr Webb <burrwebb@MacBook-Air.local>
2017-10-08 19:45:07 -0700

    playing with changes and improving code.

--- generatePassword.py
+++ generatePassword.py
@@ -5,9 +5,9 @@ import sys
 
 sys.argv
 
-numberOfWords = str(sys.argv[1])
+numberOfWords = int(sys.argv[1])
 
-numberOfWords = int(numberOfWords)
+#numberOfWords = int(numberOfWords)
 
 currentTime = int(time.time())
 randomInt = random.randint(1,1000000)
@@ -15,31 +15,35 @@ randomInt = random.randint(1,1000000)
 myHash = currentTime * randomInt
 
 random.seed(myHash)
-
+finalPassphrase = ""
 
 for z in range(numberOfWords):
-    finalCode = ""
-    finalCode = str(finalCode)
-    
-    for x in range(5):
-        
-        anInt = random.randint(1,6)
-        anInt = str(anInt)
-        finalCode = finalCode + anInt
+	finalCode = ""
+	#finalCode = str(finalCode)
+	
+	for x in range(5):
+		anInt = random.randint(1,6)
+		anInt = str(anInt)
+		finalCode = finalCode + anInt
+	
+	print "+-----------------------------------------"
+	print "|  Number: " + finalCode
 
-    print "+-----------------------------------------"
-    print "|  Number: " + finalCode
-
-
-    with open('dicewareList.csv') as csvfile:
-          reader = csv.DictReader(csvfile)
-          for row in reader:
-              idNumber = int(row["idNumber"])
-              associatedWord = str(row["associatedWord"])
-              if int(finalCode) == idNumber:
-                  print "|  Word: " + associatedWord
-                  print "+-----------------------------------------"
-                  print ""
-
+	with open('dicewareList.csv') as csvfile:
+		reader = csv.DictReader(csvfile)
+    	for row in reader:
+			idNumber = int(row["idNumber"])
+			associatedWord = str(row["associatedWord"])
+			if int(finalCode) == idNumber:
+				print "|  Word: " + associatedWord
+				print "+-----------------------------------------"
+				print ""
+    		
+    		if z == 0:
+				finalPassphrase = finalPassphrase + associatedWord
+			else:
+				finalPassphrase = finalPassphrase + " + " + associatedWord
+			
+print("Final Passphrase: " + finalPassphrase)
 
 
