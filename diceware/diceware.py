68e1e7998ebf17bd2f008a2c2c785a35ff9a106e
burrwebb <burrwebb@gmail.com>
2019-08-19 20:12:58 -0600

    updating docstrings and licensing

--- diceware/diceware.py
+++ diceware/diceware.py
@@ -1,3 +1,12 @@
+""" ===================================================================================================================
+|
+|   Name      :  diceware.py
+|   Module    :  diceware
+|   Copyright :  2019 Burr Webb
+|   License   :  MIT  (https://choosealicense.com/licenses/mit)
+|
+=================================================================================================================== """
+
 import random
 import csv
 
@@ -18,7 +27,6 @@ class Diceware:
         """
 
         self.csv_file = csv_file
-
         self.my_hash = random.randint(1, 1000000) * random.randint(1, 1000000) * random.randint(1, 1000000)
         random.seed(self.my_hash)
 
@@ -94,3 +102,23 @@ class Diceware:
             count += 1
 
         return list_of_phrases
+
+
+"""
+Copyright 2019 Burr Webb
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+""" ===================================================================================================================
+|
+|   Name      :  setup.py
+|   Module    :  diceware
+|   Copyright :  2019 Burr Webb
+|   License   :  MIT  (https://choosealicense.com/licenses/mit)
+|
+=================================================================================================================== """
+
 from setuptools import setup
 import diceware.__version__ as version_string
 
@@ -9,9 +18,29 @@ setup(
     packages=['ideal_waffle'],
     author='Burr Webb',
     author_email='burrwebb@gmail.com',
-    keywords=['example'],
+    keywords=['diceware'],
     url='https://github.com/burrwebb/ideal-waffle',
     install_requires=[
-        'requests',
+        'requests'
     ]
 )
+
+
+"""
+Copyright 2019 Burr Webb
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
